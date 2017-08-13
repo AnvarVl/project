4037337c5916c8cb69d1d7bb637089b63ea41af8
AnvarVl <vlasov_anvar_t@mail.ru>
2017-08-13 12:04:25 +0300

    update

--- nov/mail.php
+++ nov/mail.php
@@ -6,7 +6,8 @@ $sitename = "Название сайта";
 $name = trim($_GET["name"]);
 $phone = trim($_GET["phone"]);
 $text = trim($_GET["text"]);
+$email = trim($_GET["email"]);
 
 $pagetitle = "Новая заявка с сайта \"$sitename\"";
-$message = "Имя: $name \nТелефон: $phone \nТекст: $text";
+$message = "Имя: $name \nТелефон: $phone \nТекст: $email \nПочта: $email";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
